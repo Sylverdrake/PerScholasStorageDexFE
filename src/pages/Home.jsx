2ee538aa1eddb60f1a40b55047cd1f93ab13fce2
Sylverdrake <cavitevitch@gmail.com>
2023-09-04 07:08:07 -0400

    adding fetch request to Home page, json error to be resolved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () =>
 {
+    const [items, setItems] = useState(null)
+    useEffect(()=>
+    {
+        const fetchItems = async () =>
+        {
+            const response = await fetch('http://localhost:3000/api/items/')
+            const json = await response.json()
+                if (response.ok)
+                {
+                    setItems(json)
+                }
+        }
+        fetchItems()
+    }, []);
+
     return(
         <div className="home">
-            <h2>Home</h2>
+            <div className="items">
+                {items && items.map((item)=>(
+                    <p key={item._id}>{item.name}</p>
+                ))}
+            </div>
         </div>
     )
 }
