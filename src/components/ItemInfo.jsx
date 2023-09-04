59f8526132b256cc663c13122d2b93e2461370ec
Sylverdrake <cavitevitch@gmail.com>
2023-09-04 14:31:25 -0400

    added date formatting dependency + trash icon for delete

--- src/components/ItemInfo.jsx
+++ src/components/ItemInfo.jsx
@@ -1,5 +1,8 @@
 import { useItemContext } from "../hooks/useItemsContext"
 
+
+//Date FNS
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 const ItemInfo = ({item}) =>
 {
     const {dispatch} = useItemContext()
@@ -23,8 +26,8 @@ const ItemInfo = ({item}) =>
             <h4 className="itemLocation">{item.location}</h4>
             <h5 className="category">{item.category}</h5>
             <p className="description">{item.description}</p>
-            <p>{item.createdAt}</p>
-            <span onClick={handleClick}>Delete</span>
+            <p>{formatDistanceToNow(new Date(item.createdAt), {addSuffix: true})}</p>
+            <span className="material-symbols-outlined" onClick={handleClick}>Delete</span>
         </div>
     )
 }
