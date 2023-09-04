106ba27f70a832d0abe5f66e683b3526d83e6358
Sylverdrake <cavitevitch@gmail.com>
2023-09-04 13:29:08 -0400

    added local state synchronization

--- src/context/ItemContext.js
+++ src/context/ItemContext.js
@@ -1,14 +1,32 @@
 import { createContext, useReducer } from "react";
 export const ItemContext = createContext();
 
+    //Local State Synchronization 
+export const itemsReducer = (state, action) =>
+{
+    switch(action.type)
+    {
+        case 'SET_ITEMS':
+            return{
+                items: action.payload
+            }
+
+        case 'CREATE_ITEM':
+            return{
+                items: [action.payload, ...state.items]
+            }
+        
+        default:
+            return state
+    }
+}
+
 export const ItemContextProvider = ({children}) =>
 {
     const[state, dispatch] = useReducer(itemsReducer, 
         {
             items: null
         });
-
-    dispatch({type:'SET_ITEMS', payload: [{}, {}]})
 
     return(
         <ItemContext.Provider value={}>
