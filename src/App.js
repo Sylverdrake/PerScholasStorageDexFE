82d264debaf1006c8650c42ad1bda71e4f487c1f
Sylverdrake <cavitevitch@gmail.com>
2023-09-04 00:54:20 -0400

    added navbar component and homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+//Components
+import Navbar from './components/Navbar';
+
+//Pages
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
 
+    <Navbar/>
+      <div className='pages'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
