f37272afaa1ac4e4857b8357caf76d13752f7c53
Sylverdrake <cavitevitch@gmail.com>
2023-09-04 13:11:30 -0400

    finished context

--- src/context/ItemContext.js
+++ src/context/ItemContext.js
@@ -1,1 +1,11 @@
 import { createContext } from "react";
+export const ItemContext = createContext();
+
+export const ItemContextProvider = ({children}) =>
+{
+    return(
+        <ItemContext.Provider>
+            {children}
+        </ItemContext.Provider>
+    )
+}
