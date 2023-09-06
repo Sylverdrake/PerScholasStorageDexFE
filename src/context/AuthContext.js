fdcd861784f2fbad0b4d2ecb2b1da6b2ccd7a32b
Sylverdrake <cavitevitch@gmail.com>
2023-09-05 21:53:41 -0400

    react now renders login and remains login on refresh

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext()
 
@@ -22,6 +22,15 @@ export const AuthContextProvider = ({children}) =>
         {
         user: null
         })
+//Gets user from local storage, and if user exists, renders user as logged in.
+    useEffect(()=>
+    {
+        const user = JSON.parse(localStorage.getItem('user'))
+        if(user)
+        {
+            dispatch({type: 'LOGIN', payload: user})
+        }
+    }, [])
     
     console.log('AuthContext state:', state)
 
